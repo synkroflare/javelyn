4b7b7f6c20e3394ee293491f195eb2436b8090f4
wayTo <yurigarciadev@gmail.com>
2023-09-15 06:38:27 -0300

    safefeat1

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,7 @@ app.use(express.json({ limit: "25mb" }))
 app.use(express.urlencoded({ limit: "25mb", extended: true }))
 
 app.use((req, res, next) => {
+  const origin = req.get("Origin")
   if (
     origin !== "https://javelyn.vercel.app" &&
     req.ip !== "::ffff:187.39.124.191"
